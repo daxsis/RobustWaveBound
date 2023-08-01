ea4d7c1bc2a9c22510a2ec9d2e35fd9d91a0d894
Aleksandr Vinokhodov <VinokhodovAlex@gmail.com>
2023-08-01 11:22:26 -0400

    print loss after each batch

--- train.py
+++ train.py
@@ -144,12 +144,11 @@ for epoch in range(1, NUM_EPOCHS + 1):
 
         loop.set_postfix(loss=target_loss.item())
         avg_loss += target_loss.item()
-        if counter % 10 == 0:
-            print(
-                "\nEpoch {}......Step: {}/{}....... Average Loss for Epoch: {}".format(
-                    epoch, (counter + 1), len(train_loader), avg_loss / (counter + 1)
-                )
+        print(
+            "\nEpoch {}......Step: {}/{}....... Average Loss for Epoch: {}".format(
+                epoch, (counter + 1), len(train_loader), avg_loss / (counter + 1)
             )
+        )
 
     current_time = time.process_time()
     epoch_times.append(current_time - start_time)
