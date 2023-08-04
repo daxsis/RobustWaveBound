5b5ac480822721997f36591b3b29514ef400dc7b
Aleksandr Vinokhodov <VinokhodovAlex@gmail.com>
2023-08-03 20:58:54 -0400

    vae loss funciton use mse instead of bce

--- vae.py
+++ vae.py
@@ -141,13 +141,7 @@ class VariationalAutoEncoder(nn.Module):
 
 
 def loss_function(x: Tensor, x_t: Tensor, mu_x: Tensor, logvar_x: Tensor, z: Tensor):
-    assert torch.all(x >= 0) and torch.all(x <= 1), "Values in x are out of range [0,1]"
-    assert torch.all(x_t >= 0) and torch.all(
-        x_t <= 1
-    ), "Values in x_t are out of range [0,1]"
-    recon_loss = nn.functional.binary_cross_entropy(x_t, x, reduction="none").sum(
-        -1
-    )  # log(p(x|z))
+    recon_loss = nn.functional.mse_loss(x_t, x, reduction="none").sum(-1)  # log(p(x|z))
     prior = torch.distributions.Normal(
         torch.zeros_like(mu_x, device=x.get_device()),
         torch.ones_like(mu_x, device=x.get_device()),
